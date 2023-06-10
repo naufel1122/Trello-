06be5611a976ae82a1479b0c708920ddfe47a2f0
Muhammad Nofil <nofil.khan037@gmail.com>
2023-06-11 00:25:42 +0500

    push

--- app.js
+++ app.js
@@ -1,14 +1,49 @@
-function generate() {  
-    var first = document.getElementById('item');
-    var second = document.getElementById('Todo').value;
-    var item = document.createElement('li');
-    item.innerText = second;
+    let existingTicketObj = localStorage.getItem("tickets");
+
+    if (existingTicketObj) {
+        existingTicketObj = JSON.parse(existingTicketObj);
+    } else {
+        existingTicketObj = {};
+    }
+
+    let renderExistingTickets = () => {
 
 
-    first.appendChild(item);
-// console.log(text);
+        // existingTicketObj
+        // {
+        //     "Todo": [
+        //         "abc",
+        //         "sdfsdf",
+        //         "dsffsdf"
+        //     ],
+        //     "in progress": [
+        //         "jhgdhjdsf",
+        //         "dfsdf"
+        //     ]
+        // }
+
+
+
+        for (let key in existingTicketObj) {
+            console.log("key: ", key);
+
+            console.log(existingTicketObj[key]);
+
+            existingTicketObj[key].forEach((eachTicket, index) => {
+                // create tickets
+
+                let ticketDiv = document.createElement("div")
+                ticketDiv.setAttribute("draggable", "true")
+                ticketDiv.setAttribute("class", "ticket")
+                let t = document.createTextNode(eachTicket);
+                ticketDiv.appendChild(t);
+
+                let column = document.getElementById(key)
+                column.appendChild(ticketDiv)
+                // console.log("column: ", column);
+
+            })
+
+        }
     }
-    function remove() {
-        var list = document.getElementById("item");
-        list.removeChild(list.firstElementChild)
-    }
+    renderExistingTickets()
